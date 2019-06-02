74dd2bea30a9042f1acfed5a66033dba33d96d93
Felipe Flor <f.flor@vehikl.com>
2019-06-02 15:19:08 -0400

    Reduce FPS

--- src/classes/Configurations.ts
+++ src/classes/Configurations.ts
@@ -1,7 +1,7 @@
 export default class Configurations {
     public static columnCount: number = 300;
     public static rowCount: number = 150;
-    public static framesPerSecond: number = 20;
+    public static framesPerSecond: number = 15;
 
     public static get updateInterval(): number {
         return Math.floor(1000 / Configurations.framesPerSecond);
