19ea2022970c2f7cc71e25980a0782be9543fb34
Felipe Flor <f.flor@vehikl.com>
2019-06-02 23:00:44 -0400

    Add fps control

--- src/components/GameOfLifeRender.vue
+++ src/components/GameOfLifeRender.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="canvas-render">
+
+        <div class="slider-container">
+            <label for="fps-slider">FPS: {{fps}}</label>
+            <vue-slider id="fps-slider"
+                        lazy
+                        :marks="[0, 5, 10, 15, 20, 25, 30]"
+                        v-model="fps"
+                        :min="0"
+                        :max="30"
+                        interval="1"></vue-slider>
+        </div>
+
         <canvas id="canvas" height="750px" width="1500px"></canvas>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue, Watch} from 'vue-property-decorator';
     import Configurations from '@/classes/Configurations';
     import InteractiveGameOfLife from '@/classes/InteractiveGameOfLife';
+    import VueSlider from 'vue-slider-component'
+    import 'vue-slider-component/theme/antd.css'
 
-    @Component
+    @Component({
+        components: {VueSlider}
+    })
     export default class CanvasRender extends Vue {
         protected canvas?: HTMLCanvasElement;
         protected loopInterval: any;
         protected gameOfLife?: InteractiveGameOfLife;
+        protected fps: number = 15;
 
         protected mounted(): void {
+            Configurations.framesPerSecond = this.fps;
             this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
             const context: CanvasRenderingContext2D | null = this.canvas.getContext('2d');
@@ -24,12 +42,28 @@
             }
 
             this.gameOfLife = new InteractiveGameOfLife(context);
+            this.updateGameLoop();
+        }
+
+        @Watch('fps')
+        protected updateGameLoop(): void {
+            if(this.loopInterval) {
+                clearInterval(this.loopInterval)
+            }
+
+            if (this.fps === 0) {
+                return;
+            }
+
+            Configurations.framesPerSecond = this.fps;
             this.loopInterval = setInterval(() => {
                 if (this.gameOfLife) {
                     this.gameOfLife.update();
                 }
             }, Configurations.updateInterval);
         }
+
+
     }
 </script>
 
@@ -38,5 +72,10 @@
         border-radius: 2rem;
         box-shadow: 10px 10px 15px 0 rgba(0,0,0,0.4);
         margin: 3rem;
+    }
+
+    .slider-container {
+        width: 50rem;
+        margin: 3rem auto 0;
     }
 </style>
